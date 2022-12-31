9517422e5d6f1ee86fbb95e9806c2c611c0fc5be
Diogo Martins de Assis <diogoassis3301@gmail.com>
2022-12-31 14:23:34 -0300

    commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,9 @@
+function Banner() {
+    // JSX -> Como o React trabalha com a parte visual (parece HTML mas não é)
+    // Como se fosse JS puro manipulando o DOM
+    return (
+        <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+    )
+}
+
+export default Banner;
