2ef25bfd7f83b60b1aa8c129d4b28eae05ebb6f3
Diogo Martins de Assis <diogoassis3301@gmail.com>
2022-12-31 14:42:12 -0300

    css banner

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,15 @@
+import './Banner.css';
+
+function Banner() {
+    // JSX -> Como o React trabalha com a parte visual (parece HTML mas não é)
+    // Como se fosse JS puro manipulando o DOM
+
+    // Quando formos setar classes em React, usamos o className em vez de class
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+        </header>
+    )
+}
+
+export default Banner;
--- src/components/Banner/Banner.js
+++
@@ -1,9 +0,0 @@
-function Banner() {
-    // JSX -> Como o React trabalha com a parte visual (parece HTML mas não é)
-    // Como se fosse JS puro manipulando o DOM
-    return (
-        <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
-    )
-}
-
-export default Banner;
