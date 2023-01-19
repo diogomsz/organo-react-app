4aed861ad73140dc78e07cdc58111569e75d0350
Diogo Martins de Assis <diogoassis3301@gmail.com>
2023-01-19 20:53:44 -0300

    coomit

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -1,17 +1,9 @@
-import './Banner.css';
+import './Banner.css'
 
 const Banner = () => {
-    // JSX -> Como o React trabalha com a parte visual (parece HTML mas não é)
-    // Como se fosse JS puro manipulando o DOM
-
-    // Quando formos setar classes em React, usamos o className em vez de class
-    return (
-        <> 
-            <header className="banner">
-                <img src="/imagens/banner.png" alt="O banner principal da pági;na do Organo"/>
-            </header>
-        </>
-    );
+    return (<header className="cabecalho">
+        <img src='/imagens/banner.png' alt='Logo do Organo'/>
+    </header>)
 }
 
-export default Banner;
+export default Banner
