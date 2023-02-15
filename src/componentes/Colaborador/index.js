9e60e328afb6fa458e99c23aa4d1ab7309cf6bc5
Diogo Martins de Assis <diogomartinsdeassis1@gmail.com>
2023-02-14 21:19:45 -0300

    commit

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -11,27 +11,29 @@ const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar }) => {
         onClick: favoritar
     }
 
-    return (<div className="colaborador">
-        <AiFillCloseCircle 
-            size={25} 
-            className='deletar' 
-            onClick={() => aoDeletar(colaborador.id)} 
-        />
+    return (
+        <div className="colaborador">
+            <AiFillCloseCircle 
+                size={25} 
+                className='deletar' 
+                onClick={() => aoDeletar(colaborador.id)} 
+            />
 
-        <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
-            <img src={colaborador.imagem} alt={colaborador.nome} />
-        </div>
-        <div className="rodape">
-            <h4>{colaborador.nome}</h4>
-            <h5>{colaborador.cargo}</h5>
-            <div className='favoritar'>
-                {colaborador.favorito 
-                    ? <AiFillHeart {...propsfavorito} /> 
-                    : <AiOutlineHeart {...propsfavorito} />
-                }
+            <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+                <img src={colaborador.imagem} alt={colaborador.nome} />
+            </div>
+            <div className="rodape">
+                <h4>{colaborador.nome}</h4>
+                <h5>{colaborador.cargo}</h5>
+                <div className='favoritar'>
+                    {colaborador.favorito 
+                        ? <AiFillHeart {...propsfavorito} /> 
+                        : <AiOutlineHeart {...propsfavorito} />
+                    }
+                </div>
             </div>
         </div>
-    </div>)
+    )
 }
 
 export default Colaborador;
